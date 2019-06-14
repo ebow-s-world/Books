b0828dc4142bc8fbc33ae7a369ea0530a50730bc
ericbowman <ericjbowman@gmail.com>
2019-06-14 13:08:42 -0400

    Made ui.js

---
+++ assets/scripts/books/ui.js
@@ -0,0 +1,21 @@
+const showBooksTemplate = require('../templates/book-listing.handlebars')
+
+const getBooksSuccess = (data) => {
+  console.log(data)
+  const showBooksHtml = showBooksTemplate({ books: data.books })
+  $('.content').html(showBooksHtml)
+}
+
+const clearBooks = () => {
+  $('.content').empty()
+}
+
+const failure = (error) => {
+  console.error(error)
+}
+
+module.exports = {
+  getBooksSuccess,
+  clearBooks,
+  failure
+}
